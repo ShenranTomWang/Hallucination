d76c11df8c8f3366bf81316bfa835d4ccfab0eb2
shenranw <wsr.20021128@gmail.com>
2024-11-18 10:57:55 -0800

    2 agents pipeline

--- data_collection.py
+++ data_collection.py
@@ -17,17 +17,61 @@ end = END_IDX if END_IDX != -1 else len(data)
 chat_completions = []
 while count < end:
     print(count)
-    # print(data["Question"][count])
+    question = data["Question"][count]
+    answer1 = data["single_agent_response"][count]
+    refiner_prompt1 = f"{question}. Here is a response to this question: {answer1}. Please provide feedback on this answer, being specific to the question and providing actionable feedback. Ask 2 questions regarding the aspects of the response that seem incorrect."
     chat_completion = client.chat.completions.create(
         messages=[
             {
                 "role": "user",
-                "content": data["Question"][count]
+                "content": refiner_prompt1
             }
         ],
         model=MODEL_NAME
     )
-    data.loc[count, "single_agent_response"] = chat_completion.choices[0].message.content
+    time.sleep(2)
+    
+    feedback1 = chat_completion.choices[0].message.content
+    proposer_prompt2 = f"{question} Here is an initial response {answer1} and a feedback {feedback1}. Please refine this response, considering the feedback and questions."
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": proposer_prompt2
+            }
+        ],
+        model=MODEL_NAME
+    )
+    time.sleep(2)
+    
+    answer2 = chat_completion.choices[0].message.content
+    refiner_prompt2 = f"{question}. Here is the initial response: {answer1}, initial feedback: {feedback1}, and the refined answer: {answer2}. Please provide feedback on this answer, being specific to the question and providing actionable feedback. Ask 2 questions regarding the aspects of the response that seem incorrect."
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": refiner_prompt2
+            }
+        ],
+        model=MODEL_NAME
+    )
+    time.sleep(2)
+    
+    feedback2 = chat_completion.choices[0].message.content
+    proposer_prompt3 = f"{question} Here is an initial response {answer1} and an initial feedback {feedback1}. The response is then refined to {answer2}, the feedback for this response is {feedback2}. Please refine this response, considering the feedback and questions."
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": proposer_prompt3
+            }
+        ],
+        model=MODEL_NAME
+    )
+    time.sleep(2)
+    
+    final_answer = chat_completion.choices[0].message.content
+    data.loc[count, "two_agents_probing"] = final_answer
     # print(chat_completion.choices[0].message.content)
     time.sleep(2)
     count += 1
