522e1f18c4438232d0391fb6bda8b05e8fe77747
ShenranTomWang <wsr.20021128@gmail.com>
2024-11-19 18:28:09 -0500

    using one-round refine

--- data_collection.py
+++ data_collection.py
@@ -2,14 +2,13 @@ import os
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from utils.data_collection_utils import generate, get_class
-import templates
 
 MODEL = "/project/6080355/shenranw/CoT/models/google/gemma-2-2b-it"
 START_IDX = int(os.getenv("START_IDX"))
 END_IDX = int(os.getenv("END_IDX"))
 DATASET = os.getenv("DATASET")
 PROBE = int(os.getenv("PROBE")) == 1
-PromptClass = get_class("templates", "TwoAgentsProbing" if PROBE else "TwoAgentsNonProbing")
+PromptClass = get_class("templates", "Probing" if PROBE else "NonProbing")
 DatasetClass = get_class(f"data.dataset", DATASET)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -38,22 +37,10 @@ while count < end:
     
     answer2 = generate(model, tokenizer, proposer_prompt2, device)
     # print(answer2 + "\n" + "*" * 100)
-    refiner_prompt2 = PromptClass.REFINER_PROMPT_2_TEMPLATE.substitute(
+    data.write("two_agents_probing" if PROBE else "two_agents", count, answer2)
+    
+    summarizer_prompt = PromptClass.SUMMARIZER_PROMPT_TEMPLATE.substitute(
         question=question, answer1=answer1, feedback1=feedback1, answer2=answer2
-    )
-    
-    feedback2 = generate(model, tokenizer, refiner_prompt2, device)
-    # print(feedback2 + "\n" + "*" * 100)
-    proposer_prompt3 = PromptClass.PROPOSER_PROMPT_3_TEMPLATE.substitute(
-        question=question, answer1=answer1, feedback1=feedback1, answer2=answer2, feedback2=feedback2
-    )
-    
-    answer3 = generate(model, tokenizer, proposer_prompt3, device)
-    # print(answer3 + "\n" + "*" * 100)
-    data.write("two_agents_probing" if PROBE else "two_agents", count, answer3)
-    
-    summarizer_prompt = templates.SUMMARIZER_PROMPT_TEMPLATE.substitute(
-        question=question, answer1=answer1, feedback1=feedback1, answer2=answer2, feedback2=feedback2, answer3=answer3
     )
     final_answer = generate(model, tokenizer, summarizer_prompt, device)
     # print(final_answer + "\n" + "*" * 100)
--- templates.py
+++ templates.py
@@ -1,16 +1,15 @@
 from string import Template
 
-class TwoAgentsProbing():
+class Probing():
     REFINER_PROMPT_1_TEMPLATE = Template("$question. Here is a response to this question: $answer1. Please provide feedback on this answer, being specific to the question and providing actionable feedback. Ask 2 questions regarding the aspects of the response that seem incorrect.")
     PROPOSER_PROMPT_2_TEMPLATE = Template("$question Here is an initial response $answer1 and a feedback $feedback1. Please refine this response, considering the feedback and questions.")
+    SUMMARIZER_PROMPT_TEMPLATE = Template("$question Here is an initial response to this question: $answer1 Here is the initial feedback: $feedback1. Here is the second answer based on the feedback and doubts: $answer2. Please look at the chat history and provide a final response to $question based on it.")
     REFINER_PROMPT_2_TEMPLATE = Template("$question. Here is the initial response: $answer1, initial feedback: $feedback1, and the refined answer: $answer2. Please provide feedback on this answer, being specific to the question and providing actionable feedback. Ask 2 questions regarding the aspects of the response that seem incorrect.")
     PROPOSER_PROMPT_3_TEMPLATE = Template("$question Here is an initial response $answer1 and an initial feedback $feedback1. The response is then refined to $answer2, the feedback for this response is $feedback2. Please refine this response, considering the feedback and questions.")
-    
 
-class TwoAgentsNonProbing():
+class NonProbing():
     REFINER_PROMPT_1_TEMPLATE = Template("$question. Here is a response to this question: $answer1. Please provide feedback on this answer, being specific to the question and providing actionable feedback.")
     PROPOSER_PROMPT_2_TEMPLATE = Template("$question Here is an initial response $answer1 and a feedback $feedback1. Please refine this response, considering the feedback.")
+    SUMMARIZER_PROMPT_TEMPLATE = Template("$question Here is an initial response to this question: $answer1 Here is the initial feedback: $feedback1. Here is the second answer based on the feedback: $answer2. Please look at the chat history and provide a final response to $question based on it.")
     REFINER_PROMPT_2_TEMPLATE = Template("$question. Here is the initial response: $answer1, initial feedback: $feedback1, and the refined answer: $answer2. Please provide feedback on this answer, being specific to the question and providing actionable feedback.")
     PROPOSER_PROMPT_3_TEMPLATE = Template("$question Here is an initial response $answer1 and an initial feedback $feedback1. The response is then refined to $answer2, the feedback for this response is $feedback2. Please refine this response, considering the feedback.")
-    
-SUMMARIZER_PROMPT_TEMPLATE = Template("$question Here is an initial response to this question: $answer1 Here is the initial feedback: $feedback1. Here is the second answer based on the feedback and the doubts: $answer2 Here is the second feedback: $feedback2 Here is the third answer based on the two feedbacks: $answer3. Please look at the chat history and provide a final response to $question based on it.")
