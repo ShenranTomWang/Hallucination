4faaf4df48c031cc8c3b52d64172e62ba5422ddf
ShenranTomWang <wsr.20021128@gmail.com>
2024-11-20 13:52:10 -0500

    debug to use 2 rounds refinement

--- data_collection.py
+++ data_collection.py
@@ -11,6 +11,7 @@ PROBE = int(os.getenv("PROBE")) == 1
 PromptClass = get_class("templates", "Probing" if PROBE else "NonProbing")
 DatasetClass = get_class(f"data.dataset", DATASET)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Executing on device {device}")
 
 data = DatasetClass()
 
@@ -18,7 +19,9 @@ count = START_IDX
 end = END_IDX if END_IDX != -1 else data.len()
 chat_completions = []
 model = AutoModelForCausalLM.from_pretrained(MODEL, device_map=device, torch_dtype=torch.float16)
+print("model loaded")
 tokenizer = AutoTokenizer.from_pretrained(MODEL, device_map=device, torch_dtype=torch.float16)
+print("tokenizer loaded")
 while count < end:
     print(count)
     question = data.get_question(count)
@@ -49,7 +52,7 @@ while count < end:
     
     answer3 = generate(model, tokenizer, proposer_prompt3, device)
     # print(answer2 + "\n" + "*" * 100)
-    data.write("two_agents_probing" if PROBE else "two_agents", count, answer2)
+    data.write("two_agents_probing" if PROBE else "two_agents", count, answer3)
     
     summarizer_prompt = PromptClass.SUMMARIZER_PROMPT_TEMPLATE.substitute(
         question=question, answer1=answer1, feedback1=feedback1, answer2=answer2
