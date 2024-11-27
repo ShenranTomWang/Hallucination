c744f71f1c4a70103d0b091a9f1d36592c3c6bbb
ShenranTomWang <wsr.20021128@gmail.com>
2024-11-26 23:14:23 -0500

    keep feedback

--- data_collection.py
+++ data_collection.py
@@ -38,6 +38,7 @@ while count < end:
         )
         
         feedback1 = generate(model, tokenizer, refiner_prompt1, device)
+        data.write("feedback1", count, feedback1)
         # print(feedback1 + "\n" + "*" * 100)
         proposer_prompt2 = PromptClass.PROPOSER_PROMPT_2_TEMPLATE.substitute(
             question=question, answer1=answer1, feedback1=feedback1
@@ -46,7 +47,7 @@ while count < end:
         
         answer2 = generate(model, tokenizer, proposer_prompt2, device)
         # print(answer2 + "\n" + "*" * 100)
-        data.write("two_agents_probing" if PROBE else "two_agents", count, answer2)
+        data.write("two_agents", count, answer2)
         
         summarizer_prompt = PromptClass.SUMMARIZER_PROMPT_TEMPLATE.substitute(
             question=question, answer1=answer1, feedback1=feedback1, answer2=answer2
@@ -55,7 +56,7 @@ while count < end:
         # allocated_memory = torch.cuda.memory_allocated(0) / (1024 ** 3)
         # print(f"Allocated {allocated_memory} GB cuda memory")
         # print(final_answer + "\n" + "*" * 100)
-        data.write("three_agents_probing" if PROBE else "three_agents", count, final_answer)
+        data.write("three_agents", count, final_answer)
     except Exception as e:
         print(e)
     finally:
